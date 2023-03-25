29770f8949b05a3764a01b4076bb05d5c88da8f0
tianhuili4190 <123705803+tianhuili4190@users.noreply.github.com>
2023-03-24 23:38:06 -0400

    Update Part_2_main.m

--- Part_2_main.m
+++ Part_2_main.m
@@ -1,29 +1,45 @@
+clc
+clear all
 img = imread('cwheelnoise.gif');
 
 lambda = 0.25;
-K=25;% Increase K, the difference among images will be more clear.
+K=20;% Increase K, the difference among images will be more clear.
 
 
 
 
 
 %% Part 2(a)
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = (img >= min_threshold) & (img <= max_threshold);
+region = double(img) .* double(mask);
+
 figure (1)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(img)
 title('Anisotropic-diffusion results after 0 iteration (original figure)');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(img);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
+subplot(2,3,3)
+imshow(uint8(region))
+title('Spokes from 0 iterations diffusion')
+
+
+subplot(2,3,[4,5,6]);
 [c_0,bin_0] = imhist(img);
 bar(bin_0, c_0);
 xlabel('Pixel intensity');
 ylabel('Count');
 
-img=double(img);
+
+
+
+
 
 diffusion_1= anisodiff(img, K, lambda, 1);
 diffusion_2= anisodiff(img, K, lambda, 2);
@@ -39,38 +55,62 @@ for i=1:99
 
 
 if i==5 
+
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_1 >= min_threshold) & ( diffusion_1 <= max_threshold);
+region = double( diffusion_1) .* double(mask);    
+    
+    
 diffusion_1=uint8(diffusion_1);
 diffusion_2=uint8(diffusion_2);
+
 figure (2)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_1)
 title('anisotropic-diffusion results after 5 iteration for form 1');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_1);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 5 iterations for form1')
+subplot(2,3,[4,5,6]);
 [c_5,bin_5] = imhist(diffusion_1);
 bar(bin_5, c_5);
 xlabel('Pixel intensity');
 ylabel('Count');
 
 
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_2 >= min_threshold) & ( diffusion_2 <= max_threshold);
+region = double( diffusion_2) .* double(mask);  
+
+
 figure (3)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_2)
 title('anisotropic-diffusion results after 5 iteration for form 2');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_2);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 5 iterations for form 2')
+subplot(2,3,[4,5,6]);
 [c_5,bin_5] = imhist(diffusion_2);
 bar(bin_5, c_5);
 xlabel('Pixel intensity');
 ylabel('Count');
+
+
 diffusion_1=double(diffusion_1);
 diffusion_2=double(diffusion_2);
 end
@@ -78,35 +118,55 @@ end
 
 
 if i==20
+    
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_1 >= min_threshold) & ( diffusion_1 <= max_threshold);
+region = double( diffusion_1) .* double(mask);    
+    
+    
 diffusion_1=uint8(diffusion_1);
 diffusion_2=uint8(diffusion_2);
 
 figure (4)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_1)
 title('anisotropic-diffusion results after 20 iteration for form 1');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_1);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 20 iterations for form 1');
+subplot(2,3,[4,5,6]);
 [c_5,bin_5] = imhist(diffusion_1);
 bar(bin_5, c_5);
 xlabel('Pixel intensity');
 ylabel('Count');
 
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_2 >= min_threshold) & ( diffusion_2 <= max_threshold);
+region = double( diffusion_2) .* double(mask);  
+
 
 figure (5)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_2)
 title('anisotropic-diffusion results after 20 iteration for form 2');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_2);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 20 iterations for form 2');
+subplot(2,3,[4,5,6]);
 [c_5,bin_5] = imhist(diffusion_2);
 bar(bin_5, c_5);
 xlabel('Pixel intensity');
@@ -116,37 +176,59 @@ diffusion_2=double(diffusion_2);
 end
 
 if i==99  
+    
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_1 >= min_threshold) & ( diffusion_1 <= max_threshold);
+region = double( diffusion_1) .* double(mask); 
+
+
 diffusion_1=uint8(diffusion_1);
 diffusion_2=uint8(diffusion_2);     
     
 figure (6)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_1)
 title('anisotropic-diffusion results after 100 iteration for form 1');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_1);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
-[c_5,bin_5] = imhist(diffusion_1);
-bar(bin_5, c_5);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 100 iterations for form 1');
+subplot(2,3,[4,5,6]);
+subplot(2,3,[4,5,6]);
+[c_6,bin_6] = imhist(diffusion_1);
+bar(bin_6, c_6);
 xlabel('Pixel intensity');
 ylabel('Count');
 
 
+min_threshold = 95-20;
+max_threshold = 95+20;
+%create binary mask
+mask = ( diffusion_2 >= min_threshold) & ( diffusion_2 <= max_threshold);
+region = double( diffusion_2) .* double(mask);  
+
+
 figure (7)
-subplot(2,2,1);
+subplot(2,3,1);
 imshow(diffusion_2)
 title('anisotropic-diffusion results after 100 iteration for form 2');
-subplot(2,2,2)
+subplot(2,3,2)
 imshow(diffusion_2);
 hold on;
 line([1, size(img, 2)], [128, 128], 'Color', 'r', 'LineWidth', 2);
 title('y=128');
-subplot(2,2,[3,4]);
-[c_5,bin_5] = imhist(diffusion_2);
-bar(bin_5, c_5);
+subplot(2,3,3)
+imshow(uint8(region));
+title('Spokes from 100 iterations for form 2');
+subplot(2,3,[4,5,6]);
+[c_7,bin_7] = imhist(diffusion_2);
+bar(bin_7, c_7);
 xlabel('Pixel intensity');
 ylabel('Count');
 diffusion_1=double(diffusion_1);
@@ -213,3 +295,7 @@ end
 
 
 end
+
+
+
+
