3ae77e1d4a8a492e2513898d43be2812b9893fca
tianhuili4190 <123705803+tianhuili4190@users.noreply.github.com>
2023-03-25 18:41:00 -0400

    Update sigma5x5.m

--- sigma5x5.m
+++ sigma5x5.m
@@ -1,16 +1,17 @@
-%%%%%%%%%%%%% Function mean5x5 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% Function Sigma5x5 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Purpose:
-% Compute a 5X5 mean (neighborhood average) filter at each pixel
-% in an image
-% Input: M:column number;N:Row number
-% <ORIGINAL LINES 6-31 CUT FOR THIS DEMO>
+%   Compute a 5X5 Sigma filter at each pixel in an image.
+% Input: 
+%   M:column number;N:Row number; f: original image; fzero: zeros(M,N);
+% Output:
+%  sigma_5: filted image.
+% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
 function [sigma_5] = sigma5x5(f,fzero,M,N)
-
-
+sigma=20; %Given by the question
 
 % Fill the output image with zeroes first
     for x = 1 : M
@@ -21,6 +22,7 @@ function [sigma_5] = sigma5x5(f,fzero,M,N)
     
 % Convert f to a 16-bit number, so we can do sums > 255 correctly
 g = uint16(f);
+
 % Define the coordinate limits for output pixels that can be properly
 % computed by the 5X5 filter
 xlo = 3; % Can't process first two columns
@@ -30,20 +32,26 @@ yhi = N-2; % Can't process last two rows
 % Compute the filtered output image
 
 
-for x = xlo : xhi % Don't consider boundary pixels that can't
-    for y = ylo : yhi % be processed!
+for x = xlo : xhi       % Don't consider boundary pixels that can't
+    for y = ylo : yhi   %    be processed!
         num=0;
          
- 
+% Only if a difference value between the a pixel in side 5x5 filter and middle 
+%   pixel(Xk) is smaller than 2*simga, this value of pixel will be consider
+%   for computing the final mean value.
+
+  
            for i = -2 : 2
             for j = -2 : 2
-             if abs(g(x-i,y-j)-g(x,y))<=40
+             if abs(g(x-i,y-j)-g(x,y))<=(sigma*2)
                 sigma_5(x,y)= sigma_5(x,y)+g(x-i,y-j);
-                num=num+1;
+                num=num+1; %count the number of pixels 
+                           %    for final mean computation.
              end
             end
            end
-    sigma_5(x,y) = sigma_5(x,y)/ num; % Normalize by the number of elements in the 5x5 filter (5*5 = 25)         
+           
+    sigma_5(x,y) = sigma_5(x,y)/ num; % compute the mean value for 5x5 filter.   
     end
 end
 % Convert back to an 8-bit image
